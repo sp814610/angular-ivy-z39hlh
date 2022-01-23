127189d8971084c91f15529877f58ab8e034a81f
sp814610 <sp814610@gmail.com>
2022-01-23 18:42:12 +0100

    Added ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,12 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  constructor(translate: TranslateService) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('it');
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translate.use('it');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,30 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { RouterModule } from '@angular/router';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule } from '@angular/common/http';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, '../assets/i18n/core/', '.json');
+}
+
 @NgModule({
-  imports: [BrowserModule, FormsModule, RouterModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([]),
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'it',
+    }),
+  ],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
